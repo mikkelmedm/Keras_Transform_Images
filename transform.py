7a5a3da7611f892a03716f0417d5ae9bfc188c2d
Mikkel Loose <38498170+mikkelmedm@users.noreply.github.com>
2018-09-06 10:53:15 +0200

    Add files via upload

---
+++ transform.py
@@ -0,0 +1,38 @@
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import glob
+import os
+
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+# Generates an array with the images from the folder:
+img_dir = "pictures"
+data_path = os.path.join(img_dir,'*g') 
+files = glob.glob(data_path)
+data = []
+for f1 in files:
+    data.append(f1)
+
+
+for j in data:
+    img = load_img(j)  # this is a PIL image
+    x = img_to_array(img)  # this is a Numpy array with shape (300, 300, 3)
+    x = x.reshape((1,) + x.shape)  # this is a Numpy array with shape (1, 300, 300, 3)
+    x.shape
+
+# the .flow() command below generates batches of randomly transformed images
+# and saves the results to the `preview/` directory
+
+# lige nu laver den 5 transformationer af hver. Dette kan indstilles:
+
+    i = 0
+    for batch in datagen.flow(x, batch_size=1, save_to_dir='transformed', save_prefix='el', save_format='jpeg'):
+        i += 1
+        if i > 5:
+            break  # otherwise the generator would loop indefinitely
